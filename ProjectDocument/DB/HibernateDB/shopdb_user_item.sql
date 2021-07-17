2965a89c2eb6d51324e920c73258355fdaebf749
joejona <joejona124@gmail.com>
2021-07-17 17:41:20 +0000

    message

---
+++ ProjectDocument/DB/HibernateDB/shopdb_user_item.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: shopdb
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_item`
+--
+
+DROP TABLE IF EXISTS `user_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_item` (
+  `User_id` int NOT NULL,
+  `ItemInfo_id` int NOT NULL,
+  PRIMARY KEY (`User_id`,`ItemInfo_id`),
+  KEY `FKd4edp2364ipety3etqs9veb1m` (`ItemInfo_id`),
+  CONSTRAINT `FKd4edp2364ipety3etqs9veb1m` FOREIGN KEY (`ItemInfo_id`) REFERENCES `itemtb` (`ID`),
+  CONSTRAINT `FKm07awdaeiep2b8eq4wg7dktow` FOREIGN KEY (`User_id`) REFERENCES `usertb` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_item`
+--
+
+LOCK TABLES `user_item` WRITE;
+/*!40000 ALTER TABLE `user_item` DISABLE KEYS */;
+INSERT INTO `user_item` VALUES (1,1),(2,2),(1,3),(2,4),(1,5),(2,6),(1,7);
+/*!40000 ALTER TABLE `user_item` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-17 17:39:27
